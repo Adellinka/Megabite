9043430c82bfe9efc6ad1088c8e8270c3d65f8ed
Adellinka <adel.foltynkova@gmail.com>
2021-05-19 19:23:01 +0200

    safas

--- backend/server.js
+++ backend/server.js
@@ -4,15 +4,17 @@ import pubgRouter from "../backend/routers/pubgRouter.js";
 import lolRouter from "../backend/routers/lolRouter.js";
 import valorantRouter from "../backend/routers/valorantRouter.js";
 import userRouter from "./routers/userRouter.js";
-import data from "../backend/data.js";
+import dotenv from "dotenv";
 import uploadRouter from "./routers/uploadRouter.js";
 import path from "path";
+
+dotenv.config();
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-mongoose.connect("mongodb://localhost/megabite", {
+mongoose.connect(process.env.MONGO_URL || "mongodb://localhost/megabite", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
@@ -40,7 +42,7 @@ app.get("*", (req, res) =>
   res.send("Server is ready");
 }); */
 
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
   console.log(`Serve at http://localhost:${port}`);
